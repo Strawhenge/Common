8e1a8d6a117475fc0de3ff19bf725e484a361801
Matt Bristow <strawhengestudios@gmail.com>
2022-08-12 19:01:25 +0100

    added initial common .net standard assembly with build events

---
+++ Strawhenge.Common/Strawhenge.Common/ILogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Strawhenge.Common
+{
+    public interface ILogger
+    {
+        void LogInformation(string message);
+
+        void LogWarning(string message);
+
+        void LogError(string message);
+
+        void LogException(Exception exception);
+    }
+}
