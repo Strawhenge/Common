04f3933a792201a5a1bb2479ba781548cff5b265
Matt Bristow <strawhengestudios@gmail.com>
2023-01-24 20:32:02 +0000

    added IsVisible camera extension

---
+++ Strawhenge.Common.Unity/Assets/Package/Runtime/Extensions/CameraExtensions.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Strawhenge.Common.Unity
+{
+    public static class CameraExtensions
+    {
+        public static bool IsVisible(this Camera camera, Transform transform) => camera.IsVisible(transform.position);
+
+        public static bool IsVisible(this Camera camera, Vector3 point)
+        {
+            var pointToScreen = camera.WorldToViewportPoint(point);
+
+            return pointToScreen.z >= 0 &&
+                   pointToScreen.x >= 0 &&
+                   pointToScreen.x <= 1 &&
+                   pointToScreen.y >= 0 &&
+                   pointToScreen.y <= 1;
+        }
+    }
+}
