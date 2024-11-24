2031dde066a19049f6e97b42fd8638e57df09544
Matt Bristow <strawhengestudios@gmail.com>
2024-11-24 19:32:18 +0000

    added ComponentRefHelper

---
+++ Strawhenge.Common.Unity/Assets/Package/Runtime/Helpers/ComponentRefHelper.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+namespace Strawhenge.Common.Unity.Helpers
+{
+    public static class ComponentRefHelper
+    {
+        public static void EnsureSceneComponent<T>(ref T component, string fieldName, MonoBehaviour context)
+            where T : Component
+        {
+            if (component != null)
+                return;
+
+            Debug.LogWarning($"'{fieldName}' not set - locating in scene.", context);
+
+            component =
+                Object.FindObjectOfType<T>() ??
+                Object.FindObjectOfType<T>(includeInactive: true);
+
+            if (component != null)
+                return;
+
+            Debug.LogError($"'{typeof(T).Name}' not found in scene.", context);
+        }
+
+        public static void EnsureHierarchyComponent<T>(ref T component, string fieldName, MonoBehaviour context)
+            where T : Component
+        {
+            if (component != null)
+                return;
+
+            Debug.LogWarning($"'{fieldName}' not set - locating in hierarchy.", context);
+
+            component =
+                context.GetComponentInChildren<T>() ??
+                context.GetComponentInChildren<T>(includeInactive: true);
+
+            if (component != null)
+                return;
+
+            Debug.LogError($"'{typeof(T).Name}' not found in hierarchy.", context);
+        }
+
+        public static void EnsureCamera(ref UnityEngine.Camera camera, string fieldName, MonoBehaviour context)
+        {
+            if (camera != null)
+                return;
+
+            Debug.LogWarning($"'{fieldName}' not set - locating in scene.", context);
+
+            camera =
+                UnityEngine.Camera.main ??
+                Object.FindObjectOfType<UnityEngine.Camera>() ??
+                Object.FindObjectOfType<UnityEngine.Camera>(includeInactive: true);
+
+            if (camera != null)
+                return;
+
+            Debug.LogError($"'{nameof(UnityEngine.Camera)}' not found in scene.", context);
+        }
+    }
+}
