3c48232fa5d4e0d8294eb1cd82fda601f1b7ee5f
Strawhenge <strawhengestudios@gmail.com>
2025-10-07 22:21:49 +0100

    added root hierarchy method to component ref helper

--- Strawhenge.Common.Unity/Assets/Package/Runtime/Helpers/ComponentRefHelper.cs
+++ Strawhenge.Common.Unity/Assets/Package/Runtime/Helpers/ComponentRefHelper.cs
@@ -4,24 +4,6 @@ namespace Strawhenge.Common.Unity.Helpers
 {
     public static class ComponentRefHelper
     {
-        public static void EnsureSceneComponent<T>(ref T component, string fieldName, MonoBehaviour context)
-            where T : Component
-        {
-            if (component != null)
-                return;
-
-            Debug.LogWarning($"'{fieldName}' not set - locating in scene.", context);
-
-            component =
-                Object.FindObjectOfType<T>() ??
-                Object.FindObjectOfType<T>(includeInactive: true);
-
-            if (component != null)
-                return;
-
-            Debug.LogError($"'{typeof(T).Name}' not found in scene.", context);
-        }
-
         public static void EnsureHierarchyComponent<T>(ref T component, string fieldName, MonoBehaviour context)
             where T : Component
         {
@@ -38,6 +20,50 @@ namespace Strawhenge.Common.Unity.Helpers
                 return;
 
             Debug.LogError($"'{typeof(T).Name}' not found in hierarchy.", context);
+        }
+
+        public static void EnsureRootHierarchyComponent<T>(ref T component, string fieldName, MonoBehaviour context)
+            where T : Component
+        {
+            if (component != null)
+                return;
+
+            Debug.LogWarning($"'{fieldName}' not set - locating in root hierarchy.", context);
+
+            component =
+                context.GetComponentInChildren<T>() ??
+                context.GetComponentInChildren<T>(includeInactive: true);
+
+            if (component != null)
+                return;
+
+            var root = component.transform.root.gameObject;
+            component =
+                root.GetComponentInChildren<T>() ??
+                root.GetComponentInChildren<T>(includeInactive: true);
+
+            if (component != null)
+                return;
+
+            Debug.LogError($"'{typeof(T).Name}' not found in hierarchy.", context);
+        }
+
+        public static void EnsureSceneComponent<T>(ref T component, string fieldName, MonoBehaviour context)
+            where T : Component
+        {
+            if (component != null)
+                return;
+
+            Debug.LogWarning($"'{fieldName}' not set - locating in scene.", context);
+
+            component =
+                Object.FindObjectOfType<T>() ??
+                Object.FindObjectOfType<T>(includeInactive: true);
+
+            if (component != null)
+                return;
+
+            Debug.LogError($"'{typeof(T).Name}' not found in scene.", context);
         }
 
         public static void EnsureCamera(ref UnityEngine.Camera camera, string fieldName, MonoBehaviour context)
