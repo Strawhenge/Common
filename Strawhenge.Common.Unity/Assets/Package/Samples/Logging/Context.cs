44721e4a82ea29c7a406cf6f90d133cd1e11c8f0
Strawhenge <strawhengestudios@gmail.com>
2025-10-07 22:04:06 +0100

    fix sample scene

--- Strawhenge.Common.Unity/Assets/Package/Samples/Logging/Context.cs
+++ Strawhenge.Common.Unity/Assets/Package/Samples/Logging/Context.cs
@@ -1,36 +1,23 @@
 using System;
-using Strawhenge.Common.Logging;
 using Strawhenge.Common.Unity;
 using UnityEngine;
-using ILogger = Strawhenge.Common.Logging.ILogger;
 
 public class Context : MonoBehaviour
 {
     [SerializeField] LoggerScript _loggerScript;
 
-    ILogger _logger;
-
-    void Awake()
-    {
-        // var unityLogger = new UnityLogger(gameObject);
-        // var decorator = new LogLevelsDecorator(unityLogger);
-        //
-        // _loggerScript.LogLevelsDecorator = decorator;
-        // _logger = decorator;
-    }
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
-            _logger.LogInformation("Logging information.");
+            _loggerScript.Logger.LogInformation("Logging information.");
 
         if (Input.GetKeyDown(KeyCode.W))
-            _logger.LogWarning("Logging warning.");
+            _loggerScript.Logger.LogWarning("Logging warning.");
 
         if (Input.GetKeyDown(KeyCode.E))
-            _logger.LogError("Logging error.");
+            _loggerScript.Logger.LogError("Logging error.");
 
         if (Input.GetKeyDown(KeyCode.X))
-            _logger.LogException(new Exception("Logging exception."));
+            _loggerScript.Logger.LogException(new Exception("Logging exception."));
     }
 }
