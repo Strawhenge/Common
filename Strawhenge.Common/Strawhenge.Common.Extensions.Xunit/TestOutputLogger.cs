d911486e6752aa0fe8ef3ed878960ae123e355e2
Strawhenge <strawhengestudios@gmail.com>
2025-10-21 22:08:04 +0100

    added TestOutputLogger

---
+++ Strawhenge.Common/Strawhenge.Common.Extensions.Xunit/TestOutputLogger.cs
@@ -0,0 +1,24 @@
+using System;
+using Strawhenge.Common.Logging;
+using Xunit.Abstractions;
+
+namespace Strawhenge.Common.Extensions.Xunit
+{
+    public class TestOutputLogger : ILogger
+    {
+        readonly ITestOutputHelper _testOutput;
+
+        public TestOutputLogger(ITestOutputHelper testOutput)
+        {
+            _testOutput = testOutput;
+        }
+
+        public void LogInformation(string message) => _testOutput.WriteLine($"[Information] {message}");
+
+        public void LogWarning(string message) => _testOutput.WriteLine($"[Warning] {message}");
+
+        public void LogError(string message) => _testOutput.WriteLine($"[Error] {message}");
+
+        public void LogException(Exception exception) => _testOutput.WriteLine($"[Exception] {exception}");
+    }
+}
