6206d4aaaddd886a964a8539d24064ef993c82d4
Strawhenge <strawhengestudios@gmail.com>
2025-12-14 16:21:19 +0000

    update dev scene

--- Strawhenge.Common.Unity/Assets/Package/Samples/AutoAssignFields/EmotesScript.cs
+++ Strawhenge.Common.Unity/Assets/Package/Samples/AutoAssignFields/EmotesScript.cs
@@ -4,6 +4,7 @@ namespace Strawhenge.Common.Unity
 {
     public class EmotesScript : MonoBehaviour
     {
+        [SerializeField] Rigidbody _rigidbody;
         [SerializeField] InventoryScript _inventory;
         [SerializeField] LoggerScript _logger;
     }
--- Strawhenge.Common.Unity/Assets/Package/Samples/AutoAssignFields/PlayerScript.cs
+++ Strawhenge.Common.Unity/Assets/Package/Samples/AutoAssignFields/PlayerScript.cs
@@ -4,6 +4,7 @@ namespace Strawhenge.Common.Unity
 {
     public class PlayerScript : MonoBehaviour
     {
+        [SerializeField] Rigidbody _rigidbody;
         [SerializeField] EmotesScript _emotes;
         [SerializeField] InventoryScript _inventory;
         [SerializeField] LoggerScript _logger;
